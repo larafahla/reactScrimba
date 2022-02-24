2e6cd17b7af2a145035fd08229f62b80f49c2f1c
larafahla <larasantosfahla@gmail.com>
2022-02-24 20:58:29 +0000

    add

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+
+
+import React from "react"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+import './App.css'
+
+function App() {
+    return (
+        <div className="Container">
+            <Navbar />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
